872b45570ae3af515b1c9432fc84213370c9b84a
Lyam02 <sauvage.lyam@outlook.fr>
2025-01-29 23:06:51 +0100

    debut de la page d'acceuil

--- src/Morpion.java
+++ src/Morpion.java
@@ -1,10 +1,11 @@
+
 import Vue.MorpionMenu;
+
 
 public class Morpion {
     public static void main(String[] args) {
         MorpionMenu unMorpionMenu =  new MorpionMenu();
 
-        
     }
 }
 
--- src/Vue/MorpionMenu.java
+++ src/Vue/MorpionMenu.java
@@ -1,6 +1,7 @@
 package Vue;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -8,17 +9,23 @@ public class MorpionMenu extends JFrame implements ActionListener {
 
     private JButton btPlay = new JButton("Play");
     private JButton btSettings = new JButton("Settings");
+    private JPanel panelForm = new JPanel();
 
 
 
+    public MorpionMenu()
+    {
 
-    public MorpionMenu() {
-        super("Morpion");
+        this.setTitle("Morpion");
+        this.setBounds(100, 100, 1500, 850);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.getContentPane().setBackground(Color.DARK_GRAY);
+        this.setLayout(null);
+        this.setResizable(false);
 
-        setVisible(true);
+        this.setVisible(true);
     }
 
-    
 
     @Override
     public void actionPerformed(ActionEvent e) {
